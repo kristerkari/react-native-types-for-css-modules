e838fcd340862faf0181a05d3155ebec4f42cb6d
Jason Feng <solidzoro@live.com>
2021-07-15 14:17:12 +0800

    fix: fixed Regular Expression compatible @types/react-native v0.64.11+

--- add_classname.rb
+++ add_classname.rb
@@ -1,4 +1,6 @@
 ARGF.each do |line|
   puts line
-  puts "", "    className?: string;" if line =~ /style\?: StyleProp<.*Style>;/
+  # puts "", "    className?: string;" if line =~ /style\?: StyleProp<.*Style>;/
+  # Since @types/react-native v0.64.11, `style` is added to `undefined`.
+  puts "", "    className?: string;" if line =~ /style\?: StyleProp<.*Style> | undefined;/
 end
